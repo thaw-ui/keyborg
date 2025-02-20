ccc846435299361faa9d6bf64bcefb7a0b1f6e9f
luoxiao <luoxiaozero@163.com>
2025-02-20 14:29:23 +0800

    feat: implement KeyborgCore::new

--- src/focus_event.rs
+++ src/focus_event.rs
@@ -1,4 +1,5 @@
 use wasm_bindgen::prelude::*;
+use web_sys::Window;
 
 pub const KEYBORG_FOCUSIN: &'static str = "keyborg:focusin";
 
@@ -15,4 +16,54 @@ extern "C" {
 
     #[wasm_bindgen(method, getter, js_name = originalEvent)]
     pub fn original_event(this: &KeyborgFocusInEventDetails) -> web_sys::FocusEvent;
+}
+
+/**
+ * Removes keyborg event listeners and custom focus override
+ * @param win The window that stores keyborg focus events
+ */
+pub fn dispose_focus_event(win: Window) {
+    // const kwin = win as WindowWithKeyborgFocusEvent;
+    // const proto = kwin.HTMLElement.prototype;
+    // const origFocus = (proto.focus as KeyborgFocus).__keyborgNativeFocus;
+    // const keyborgNativeFocusEvent = kwin.__keyborgData;
+
+    // if (keyborgNativeFocusEvent) {
+    //   kwin.document.removeEventListener(
+    //     "focusin",
+    //     keyborgNativeFocusEvent.focusInHandler,
+    //     true,
+    //   );
+
+    //   kwin.document.removeEventListener(
+    //     "focusout",
+    //     keyborgNativeFocusEvent.focusOutHandler,
+    //     true,
+    //   );
+
+    //   for (const shadowRootWeakRef of keyborgNativeFocusEvent.shadowTargets) {
+    //     const shadowRoot = shadowRootWeakRef.deref();
+
+    //     if (shadowRoot) {
+    //       shadowRoot.removeEventListener(
+    //         "focusin",
+    //         keyborgNativeFocusEvent.focusInHandler,
+    //         true,
+    //       );
+    //       shadowRoot.removeEventListener(
+    //         "focusout",
+    //         keyborgNativeFocusEvent.focusOutHandler,
+    //         true,
+    //       );
+    //     }
+    //   }
+
+    //   keyborgNativeFocusEvent.shadowTargets.clear();
+
+    //   delete kwin.__keyborgData;
+    // }
+
+    // if (origFocus) {
+    //   proto.focus = origFocus;
+    // }
 }
--- src/keyborg.rs
+++ src/keyborg.rs
@@ -2,19 +2,23 @@ use gloo_events::{EventListener, EventListenerOptions};
 use js_sys::Reflect;
 use send_wrapper::SendWrapper;
 use std::{
-    collections::HashMap,
+    collections::{HashMap, HashSet},
     sync::{Arc, OnceLock, RwLock},
 };
 use wasm_bindgen::{prelude::Closure, JsValue};
 use web_sys::{
     wasm_bindgen::{JsCast, UnwrapThrowExt},
-    Event, Window,
+    Event, HtmlElement, KeyboardEvent, Window,
 };
 
-use crate::focus_event::KEYBORG_FOCUSIN;
+use crate::focus_event::{dispose_focus_event, KEYBORG_FOCUSIN};
 
 static LAST_ID: OnceLock<RwLock<usize>> = OnceLock::new();
 static KEYBORG_MAP: OnceLock<RwLock<Option<KeyborgMap>>> = OnceLock::new();
+
+// When a key from dismiss_keys is pressed and the focus is not moved
+// during DISMISS_TIMEOUT time, dismiss the keyboard navigation mode.
+const DISMISS_TIMEOUT: i32 = 500;
 
 struct KeyborgMap {
     core: Arc<KeyborgCore>,
@@ -64,23 +68,56 @@ impl IsNavigatingWithKeyboard {
     }
 }
 
+#[derive(Debug, Clone)]
+pub struct KeyborgProps {
+    // Keys to be used to trigger keyboard navigation mode. By default, any key will trigger
+    // it. Could be limited to, for example, just Tab (or Tab and arrow keys).
+    trigger_keys: Option<Vec<u32>>,
+    // Keys to be used to dismiss keyboard navigation mode using keyboard (in addition to
+    // mouse clicks which dismiss it). For example, Esc could be used to dismiss.
+    dismiss_keys: Option<Vec<u32>>,
+}
+
 struct KeyborgCore {
     id: String,
     win: SendWrapper<Window>,
 
     is_mouse_or_touch_used_timer: Arc<RwLock<Option<i32>>>,
+    dismiss_timer: Arc<RwLock<Option<i32>>>,
+    trigger_keys: Arc<Option<HashSet<u32>>>,
+    dismiss_keys: Arc<Option<HashSet<u32>>>,
     is_navigating_with_keyboard: Arc<RwLock<IsNavigatingWithKeyboard>>,
 
-    listener_list: Vec<SendWrapper<EventListener>>,
+    _listener_list: Vec<SendWrapper<EventListener>>,
 }
 
 impl KeyborgCore {
-    pub fn new(win: Window) -> Self {
+    pub fn new(win: Window, props: Option<KeyborgProps>) -> Self {
         let last_id = LAST_ID.get_or_init(Default::default);
         let id = *last_id.read().unwrap_throw() + 1;
         *last_id.write().unwrap_throw() = id;
 
+        let mut dismiss_keys = None::<HashSet<u32>>;
+        let mut trigger_keys = None::<HashSet<u32>>;
+
+        if let Some(props) = props {
+            if let Some(keys) = props.trigger_keys {
+                if !keys.is_empty() {
+                    trigger_keys = Some(HashSet::from_iter(keys));
+                }
+            }
+
+            if let Some(keys) = props.dismiss_keys {
+                if !keys.is_empty() {
+                    dismiss_keys = Some(HashSet::from_iter(keys));
+                }
+            }
+        }
+        let dismiss_keys = Arc::new(dismiss_keys);
+        let trigger_keys = Arc::new(trigger_keys);
+
         let is_mouse_or_touch_used_timer = Arc::new(RwLock::new(None::<i32>));
+        let dismiss_timer = Arc::new(RwLock::new(None::<i32>));
         let is_navigating_with_keyboard = Arc::new(RwLock::new(IsNavigatingWithKeyboard::new()));
         let mut listener_list = vec![];
 
@@ -190,18 +227,116 @@ impl KeyborgCore {
         let listener = EventListener::new_with_options(&doc, "mousedown", options, on_mouse_down);
         listener_list.push(SendWrapper::new(listener));
 
+        let should_dismiss_keyboard_navigation = {
+            let dismiss_keys = dismiss_keys.clone();
+            move |e: &KeyboardEvent| {
+                dismiss_keys
+                    .as_ref()
+                    .clone()
+                    .is_some_and(|keys| keys.contains(&e.key_code()))
+            }
+        };
+
+        let schedule_dismiss = {
+            let win = win.clone();
+            let dismiss_timer = dismiss_timer.clone();
+            let is_navigating_with_keyboard = is_navigating_with_keyboard.clone();
+            move || {
+                if let Some(timer) = dismiss_timer.read().unwrap_throw().clone() {
+                    win.clear_timeout_with_handle(timer);
+                }
+
+                let was = win.document().unwrap_throw().active_element();
+
+                let closure = Closure::once({
+                    let dismiss_timer = dismiss_timer.clone();
+                    let is_navigating_with_keyboard = is_navigating_with_keyboard.clone();
+                    let win = win.clone();
+                    move || {
+                        dismiss_timer.write().unwrap_throw().take();
+                        let cur = win.document().unwrap_throw().active_element();
+
+                        let Some(was) = was else {
+                            return;
+                        };
+
+                        let Some(cur) = cur else {
+                            return;
+                        };
+
+                        if was == cur {
+                            // Esc was pressed, currently focused element hasn't changed.
+                            // Just dismiss the keyboard navigation mode.
+                            is_navigating_with_keyboard
+                                .write()
+                                .unwrap_throw()
+                                .set(false);
+                        }
+                    }
+                });
+
+                let id = win
+                    .set_timeout_with_callback_and_timeout_and_arguments_0(
+                        closure.as_ref().unchecked_ref::<js_sys::Function>(),
+                        DISMISS_TIMEOUT,
+                    )
+                    .unwrap_throw();
+                *dismiss_timer.write().unwrap_throw() = Some(id);
+            }
+        };
+
+        // @returns whether the keyboard event should trigger keyboard navigation mode
+        let should_trigger_keyboard_navigation = {
+            let win = win.clone();
+            let trigger_keys = trigger_keys.clone();
+            move |e: &KeyboardEvent| {
+                // TODO Some rich text fields can allow Tab key for indentation so it doesn't
+                // need to be a navigation key. If there is a bug regarding that we should revisit
+                if e.key() == "Tab" {
+                    return true;
+                }
+
+                let active_element = win.document().unwrap_throw().active_element();
+
+                let is_trigger_key = trigger_keys
+                    .as_ref()
+                    .clone()
+                    .map_or(true, |keys| keys.contains(&e.key_code()));
+
+                let is_editable = if let Some(Ok(active_element)) =
+                    active_element.map(|el| el.dyn_into::<HtmlElement>())
+                {
+                    if ["INPUT".to_string(), "TEXTAREA".to_string()]
+                        .contains(&active_element.tag_name())
+                    {
+                        true
+                    } else if active_element.is_content_editable() {
+                        true
+                    } else {
+                        false
+                    }
+                } else {
+                    false
+                };
+
+                is_trigger_key && !is_editable
+            }
+        };
+
         let on_key_down = {
             let is_navigating_with_keyboard = is_navigating_with_keyboard.clone();
 
             move |event: &Event| {
+                let e = event.dyn_ref::<web_sys::KeyboardEvent>().unwrap_throw();
+
                 if is_navigating_with_keyboard.read().unwrap_throw().get() {
-                    //   if (this._shouldDismissKeyboardNavigation(e)) {
-                    //     this._scheduleDismiss();
-                    //   }
+                    if should_dismiss_keyboard_navigation(e) {
+                        schedule_dismiss();
+                    }
                 } else {
-                    //   if (this._shouldTriggerKeyboardNavigation(e)) {
-                    //     this.isNavigatingWithKeyboard = true;
-                    //   }
+                    if should_trigger_keyboard_navigation(e) {
+                        is_navigating_with_keyboard.write().unwrap_throw().set(true);
+                    }
                 }
             }
         };
@@ -228,8 +363,11 @@ impl KeyborgCore {
             id: format!("c{id}"),
             win: SendWrapper::new(win),
             is_mouse_or_touch_used_timer,
+            dismiss_timer,
+            trigger_keys,
+            dismiss_keys,
             is_navigating_with_keyboard,
-            listener_list,
+            _listener_list: listener_list,
         }
     }
 }
@@ -239,6 +377,7 @@ impl Drop for KeyborgCore {
         let Self {
             win,
             is_mouse_or_touch_used_timer,
+            dismiss_timer,
             ..
         } = self;
 
@@ -246,12 +385,11 @@ impl Drop for KeyborgCore {
             win.clear_timeout_with_handle(timer);
         }
 
-        //   if (this._dismissTimer) {
-        //     win.clearTimeout(this._dismissTimer);
-        //     this._dismissTimer = undefined;
-        //   }
+        if let Some(timer) = dismiss_timer.read().unwrap_throw().clone() {
+            win.clear_timeout_with_handle(timer);
+        }
 
-        //   disposeFocusEvent(win);
+        dispose_focus_event(win.clone().take());
     }
 }
 
@@ -271,7 +409,7 @@ impl Keyborg {
             .for_each(|callback| callback(is_navigating_with_keyboard));
     }
 
-    pub fn create(win: Window) -> Arc<RwLock<Self>> {
+    pub fn create(win: Window, props: Option<KeyborgProps>) -> Arc<RwLock<Self>> {
         let keyborg = Arc::new(RwLock::new(Self::new()));
         let id = { keyborg.read().unwrap_throw().id.clone() };
 
@@ -279,7 +417,7 @@ impl Keyborg {
             let keyborg = keyborg.clone();
             let id = id.clone();
             move || {
-                let core = Arc::new(KeyborgCore::new(win));
+                let core = Arc::new(KeyborgCore::new(win, props));
                 {
                     let mut keyborg = keyborg.write().unwrap_throw();
                     keyborg.core = Some(core.clone());
@@ -344,12 +482,24 @@ impl Keyborg {
 
     /// @returns Whether the user is navigating with keyboard
     pub fn is_navigating_with_keyboard(&self) -> bool {
-        // return !!this._core?.isNavigatingWithKeyboard;
-        todo!()
+        if let Some(core) = &self.core {
+            core.is_navigating_with_keyboard.read().unwrap_throw().get()
+        } else {
+            false
+        }
     }
 
     /// callback - Called when the keyboard navigation state changes
     pub fn subscribe(&mut self, callback: impl Fn(bool) + Send + Sync + 'static) {
         self.cb.push(Box::new(callback));
+    }
+
+    /// @param callback - Registered with subscribe
+    pub fn unsubscribe(callback: KeyborgCallback) {
+        // const index = this._cb.indexOf(callback);
+
+        // if (index >= 0) {
+        //     this._cb.splice(index, 1);
+        // }
     }
 }
